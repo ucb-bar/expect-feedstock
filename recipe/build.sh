6de4ef9c6dcc736f495e3f9778cfcabb49cc35db
Jim Fang <yf328@eecs.berkeley.edu>
2026-09-05 07:53:32 -0700

    fix build failures

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,12 @@ autoconf
 ( cd testsuite
   autoconf -I.. )
 
+# Expect 5.45.4 ships an old config.sub that does not recognize conda
+# triplets such as aarch64-conda-linux-gnu / x86_64-conda-linux-gnu.
+find . -name config.sub -o -name config.guess | while read -r f; do
+  cp "${BUILD_PREFIX}/share/gnuconfig/$(basename "${f}")" "${f}"
+done
+
 ./configure --prefix=$PREFIX --build=$BUILD --host=$HOST --with-tclinclude=$PREFIX/include
 make -j ${CPU_COUNT}
 make test
