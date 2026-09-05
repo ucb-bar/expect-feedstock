1e00b28ca710f57fb660fa9b68e187c3e3ed917f
Jim Fang <yf328@eecs.berkeley.edu>
2026-09-05 08:04:21 -0700

    fix build failures

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,11 @@ find . -name config.sub -o -name config.guess | while read -r f; do
   cp "${BUILD_PREFIX}/share/gnuconfig/$(basename "${f}")" "${f}"
 done
 
+# GCC 14+ errors on implicit int / implicit function declarations. Expect's
+# configure tests are K&R C, so those failures leave PTY_TYPE empty and
+# configure looks for pty_.c. Keep the tests compiling.
+export CFLAGS="${CFLAGS} -fpermissive -Wno-implicit-int -Wno-implicit-function-declaration"
+
 ./configure --prefix=$PREFIX --build=$BUILD --host=$HOST --with-tclinclude=$PREFIX/include
 make -j ${CPU_COUNT}
 make test
